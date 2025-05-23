9fef0d824d9b0af03b9ee864a5b3ba4feee8531a
pw.olejnik <pw.olejnik@student.uw.edu.pl>
2025-05-23 22:30:11 +0200

    Setup PDB structures download

--- src/constants.py
+++ src/constants.py
@@ -10,3 +10,7 @@ HACKATHON_DATA = DATA_DIR / "hackathon-data(in).csv"
 STRUCTURES_DIR = DATA_DIR / "structures"
 ALPHAFOLD_STRUCTURES_DIR = STRUCTURES_DIR / "alphafold"
 PDB_STRUCTURES_DIR = STRUCTURES_DIR / "pdb"
+
+# PDB and UniProt structure IDs
+ALPHAFOLD_STRUCTURES_IDS = STRUCTURES_DIR / "alphafold_structure_ids.txt"
+PDB_STRUCTURES_IDS = STRUCTURES_DIR / "pdb_structure_ids.txt"
